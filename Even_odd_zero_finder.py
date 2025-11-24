48e30c7651fe0f7d3dd707b4002c44889a6edd88
Udaya Nath singh <udaynathsingh2003@gmail.com>
2025-11-25 01:14:33 +0530

    Add files via upload

---
+++ Even_odd_zero_finder.py
@@ -0,0 +1,7 @@
+number = int (input("Enter your number : "))
+if number  == 0 :
+    print ("It is zero")
+elif number % 2 == 0 :
+    print ("It is a even number  ")
+else:
+    print ("It is a odd number  ")
