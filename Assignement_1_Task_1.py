e9b1c0b46010240c9296c16b67d1eecf71dc3f40
Udaya Nath singh <udaynathsingh2003@gmail.com>
2025-12-03 17:17:46 +0530

    Delete Assignement_1_Task_1.py

--- Assignement_1_Task_1.py
+++
@@ -1,7 +0,0 @@
-a = float (input ( "Enter the first number :") )
-b = float (input ("Enter your second number : " ) )
-c = a/ b
-print ("addition :", a +b )
-print ("subtraction :", a -b )
-print ("multiplication :", a *b )
-print ("division :", round(c , 3) )
